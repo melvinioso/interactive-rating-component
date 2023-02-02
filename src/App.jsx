140b2a0a267ef23b862759e428261a5f0eeedca7
Matt McGavern <matt.mcgavern@gmail.com>
2023-02-01 22:54:19 -0600

    rating component styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Rating from './components/Rating';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Rating />
     </div>
   );
 }
---
+++ src/components/Rating/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Rating = (props) => {
+  return (
+    <div>
+      Rating
+    </div>
+  )
+};
+
+export default Rating;
